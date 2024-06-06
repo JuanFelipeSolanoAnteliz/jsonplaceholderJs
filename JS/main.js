dc3aa35af354602b7bd9fd5bad8669e64d1e7844
JuanFelipeSolanoAnteliz <juanfesolant0906@gmail.com>
2024-06-06 08:19:42 -0500

    feat: :sparkles: funcion 'Delete'

--- JS/main.js
+++ JS/main.js
@@ -2,6 +2,6 @@ import { getAllbums, addAlbum, deleteAlbums } from "./module/albums.js";
 import { getUser } from "./module/user.js";
 import { addPost } from "./module/post.js";
 
-
 // console.table( await addPost({userId:"1", title:"MAÑANA NO HAY CLASE", body:"LO DIJO MIGUEL FR"}));
 // console.table(await getUser({userId:10}));
+console.log( await deleteAlbums({id:"9f78"}))
--- JS/module/albums.js
+++ JS/module/albums.js
@@ -28,3 +28,25 @@ export const addAlbum = async(arg)=>{
     return data;
 };
 
+const validateDeleteAlbum =({id})=>{
+    if(typeof id !== "string" || id === undefined){
+        return {status: 406, 
+        message: `The id that was provided doesn't meet the correct argument to be found.`};    
+    };
+}
+export const deleteAlbums = async(arg)=>{
+    let val = validateDeleteAlbum(arg);
+    if(val)return val;
+    let config ={
+        method:"DELETE",
+        headers:{"content-type": "application/json"}
+    }
+
+    let res = await fetch(`http://172.16.101.146:5802/albums/${arg.id}`,config);
+    if(res.status === 404)return `id that was provided isn't registred in the database.`
+    
+    let data = await res.json();
+    data.status = 202
+    data.message = `The album ${arg.id} was deleted correctly from de database.`
+    return data;
+}
