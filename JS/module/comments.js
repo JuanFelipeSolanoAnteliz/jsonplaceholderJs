6925a2109c5298e7ebc10b01b405ce0f05d49295
JuanFelipeSolanoAnteliz <juanfesolant0906@gmail.com>
2024-06-06 20:14:55 -0500

    feat: :sparkles: Delete in comments
    add the delete function in comments, it isn't test yet due to the database isn't working

--- JS/module/comments.js
+++ JS/module/comments.js
@@ -33,3 +33,30 @@ export const addComment = async (arg) =>{
 };
 
 
+export const validateDeleteComments = async ({idComment})=>{
+    if(typeof idComment !== "string" || idComment === undefined)
+        return{
+        status: 406, 
+        message: `The id that was provided doesn't meet the correct argument to be found.`
+        };    
+};
+
+export const deleteAlbums =async(fireId)=>{
+    let val = validateDeleteComments(fireId);
+    if(val) return val;
+    let config ={
+        method:"DELETE",
+        headers:{"content-type": "application/json"}
+    };
+
+    let res = await fetch(`http://172.16.101.146:5801/comments/${fireId.idComment}`,config);
+    if(res.status === 404)return `id that was provided isn't registred in the database.`
+    
+    let data = await res.json();
+    data.status = 202;
+    data.message = `The album ${fireId.idComment} was deleted correctly from de database.`
+    return data;
+};
+
+
+
